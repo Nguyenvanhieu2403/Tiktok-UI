f21013de8e11eef9e51b5c26bdcc87f2774f7bde
Nguyenvanhieu2403 <Nguyenvanhieu2422003@gmail.com>
2024-03-08 21:34:33 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,14 @@
+import Header from '~/components/Layout/component/Header';
+
+function DefaulLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaulLayout;
